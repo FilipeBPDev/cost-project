8517e06bbb122abdca3692845c73eae7222fd069
Filipe <devfilipebp@gmail.com>
2023-10-12 12:17:23 -0300

    :sparkles: Create a deashboard

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,8 @@
 import { useLocation } from "react-router-dom";
 import Message from "../layout/Message/Message";
+import styles from './Projects.module.css';
+import Container from "../layout/Container/Container";
+import LinkButton from "../layout/LinkButton/LinkButton";
 
 function Projects() {
     const location = useLocation();
@@ -10,9 +13,15 @@ function Projects() {
     }
 
     return (
-        <div>
-            <h1>meus projetos</h1>
+        <div className={styles.projectContainer}>
+            <div className={styles.titleContainer}>
+                <h1>Meus projetos</h1>
+                <LinkButton to="/newproject" text="visualizar projeto"></LinkButton>
+            </div>
             {message && <Message type="sucess" msg={message} />}
+            <Container customClass="start">
+                <p>Projetos</p>
+            </Container>
         </div>
     );
 }
